4c56dab6eefeadac274b82d875f69b99aec870a4
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-17 15:04:38 +0300

    Add files via upload

---
+++ ft_putstr_fd.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dakcakoc <dakcakoce@student.hive.fi>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 14:41:54 by dakcakoc          #+#    #+#             */
+/*   Updated: 2024/04/17 14:48:30 by dakcakoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	write(fd, s, ft_strlen(s));
+}
+/*
+ int main() {
+     char *message = "Hello, world!\n";
+     int fd = 1;
+     ft_putstr_fd(message, fd);
+     return 0;
+ }
+ */
