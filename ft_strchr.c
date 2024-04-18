0c309f4ff9c7dc8ae47a649a69edb08934d60269
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-18 13:16:58 +0300

    Create ft_strchr.c

---
+++ ft_strchr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+char *ft_strchr(const char *s, int c) 
+{
+    const unsigned char *str =(const unsigned char *)s;
+    unsigned char b = (unsigned char)c;  // Cast the search character to unsigned char
+
+    while (*str != '\0') // Iterate through the string looking for 'b' or the end of the string
+    {
+        if (*str == b) 
+        {
+            return (char *)str;  // Correct casting back to char* when returning
+        }
+        if (b == '\0') 
+        {
+            return (char *)str; // Also cast back to char* for the null terminator
+        }
+        str++;
+    }
+    return NULL; // If not found, return NULL
+}
+
+int main()
+{
+    char str[] = "My name is Dilara";
+    printf("%s", ft_strchr(str, 'a'));
+    return 0;
+}
