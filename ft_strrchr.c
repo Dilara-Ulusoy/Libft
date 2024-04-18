ff413abb39311feb581ea5704cada4b15d79f252
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-18 14:03:45 +0300

    Create ft_strrchr.c

---
+++ ft_strrchr.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <stdio.h>
+char *ft_strrchr(const char *s, int c) 
+{
+    const unsigned char *str =(const unsigned char *)s;
+    unsigned char b = (unsigned char)c;  // Cast the search character to unsigned char
+    const unsigned char *lastoccur = NULL;
+
+    while (*str != '\0') 
+    // Iterate through the string looking for 'b' or the end of the string
+    {
+        if (*str == b) 
+        {
+            lastoccur = str;
+        }
+        if (b == '\0') 
+        {
+            return (char *)str;
+        }
+        str++;
+        
+    }
+    return(char *)lastoccur;
+}
+
+int main()
+{
+    char str1[] = "My name is Dilara";
+    printf("String 1: %s\n", ft_strrchr(str1, 'a'));
+    
+    char str2[] = "My name is Dilara";
+    printf("String 2: %s",  strrchr(str2, 'a'));
+    return 0;
+}
