6410141700655e8608f25c60893dd16a51887739
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-19 12:36:13 +0300

    Add files via upload

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,27 +1,43 @@
-void ft_putnbr_fd(int n, int fd)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dakcakoc <dakcakoce@student.hive.fi>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 09:45:29 by dakcakoc          #+#    #+#             */
+/*   Updated: 2024/04/19 09:48:14 by dakcakoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
 {
-    char digit;
-    if(n == -2147483648)
-    {
-        ft_putstr_fd("-2147483648", fd);
+	char	digit;
+
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n >= 10 && n != -2147483648)
+	{
+		ft_putnbr_fd(n / 10, fd);
+        ft_putnbr_fd(n % 10, fd);
     }
-    
-    if(n < 0)
+    else if (n != -2147483648)
     {
-        ft_putchar_fd('-', fd);
-        n = -n;
+        digit = (n % 10) + '0';
+        ft_putchar_fd(digit, fd);
     }
-    if(n >= 10)
-    {
-        ft_putnbr_fd(n/10, fd);
-    }
-   digit = (n % 10) + '0';
-   ft_putchar_fd(digit, fd);
 }
 /*
 int main()
 {
     int fd = 1;
     ft_putnbr_fd(42,fd);
-}
-*/
+}*/
