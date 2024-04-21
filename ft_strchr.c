f64aa09b5e1edbbc9e124caecbcc37d3cec4a60b
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-21 20:12:15 +0300

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,22 +1,23 @@
+ #include "libft.h"
 
-char *ft_strchr(const char *s, int c) 
+char *ft_strchr(const char *s, int c)
 {
     const unsigned char *str =(const unsigned char *)s;
-    unsigned char b = (unsigned char)c;  // Cast the search character to unsigned char
+    unsigned char b = (unsigned char)c;
 
-    while (*str != '\0') // Iterate through the string looking for 'b' or the end of the string
+    while (*str != '\0')
     {
-        if (*str == b) 
+        if (*str == b)
         {
             return (char *)str;  // Correct casting back to char* when returning
         }
-        if (b == '\0') 
-        {
-            return (char *)str; // Also cast back to char* for the null terminator
-        }
         str++;
     }
-    return NULL; // If not found, return NULL
+    if (b == '\0')
+    {
+        return (char *)str; // Also cast back to char* for the null terminator
+    }
+    return (0);
 }
 /*
 #include <stdio.h>
