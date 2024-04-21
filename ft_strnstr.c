f3219b6c1f018567c05373e88909d4cfdb621d35
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-21 21:13:13 +0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,13 +1,15 @@
- char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+#include "libft.h"
+
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
-	j = 0;
+    j = 0;
 	if (needle[j] == '\0')
 	{
-		return (haystack);
+        return ((char *)haystack); //We match the return type of the function
 	}
 	while (haystack[i] != '\0' && i < len)
 	{
@@ -15,8 +17,16 @@
 		while (haystack[i + j] != '\0' && haystack[i + j] == needle[j])
 			j++;
 		if (needle[j] == '\0')
-			return (haystack + i);
+            return ((char *)(haystack + i));
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
+/*
+#include <stdio.h>
+int main()
+{
+    char str[] = "This is an apple!";
+    char needle[]= "is";
+    printf("%s", ft_strnstr(str,needle,4));
+}*/
