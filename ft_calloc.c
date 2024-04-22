ee10737bed9e9ced8bd53a15aa97faef7f765338
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-22 10:08:33 +0300

    Add files via upload

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,23 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dakcakoc <dakcakoce@student.hive.fi>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 10:02:28 by dakcakoc          #+#    #+#             */
+/*   Updated: 2024/04/22 10:08:04 by dakcakoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 
-void *ft_calloc(size_t nitems, size_t size)
+void	*ft_calloc(size_t nitems, size_t size)
 {
-    size_t i = 0;
-    size_t totalsize = size * nitems;
-    
-    if(size == 0 || nitems == 0)
-    {
-        return NULL;
-    }
-    char *outcome;
-    outcome = malloc(totalsize);
-    if (!outcome)
-		return (NULL);
-	
-	while(i < totalsize)
+	size_t	i;
+	size_t	totalsize;
+	char	*outcome;
+
+	totalsize = size * nitems;
+	i = 0;
+	if (size == 0 || nitems == 0)
 	{
-	   outcome[i] = 0;
-	   i++;
+		return (NULL);
 	}
-	return outcome;
+	outcome = malloc(totalsize);
+	if (!outcome)
+		return (NULL);
+	while (i < totalsize)
+	{
+		outcome[i] = 0;
+		i++;
+	}
+	return (outcome);
 }
