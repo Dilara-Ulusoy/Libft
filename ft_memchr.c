7327ad6021f222418e1a9be7173bdf00ee6f1409
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-22 14:27:38 +0300

    Add files via upload

---
+++ ft_memchr.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dakcakoc <dakcakoce@student.hive.fi>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 13:51:37 by dakcakoc          #+#    #+#             */
+/*   Updated: 2024/04/22 14:26:53 by dakcakoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*str;
+	unsigned char		b;
+	size_t				i;
+
+	str = (const unsigned char *)s;
+	b = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == b)
+		{
+			return ((void *)&str[i]);
+		}
+		i++;
+	}
+	if (b == '\0')
+	{
+		return ((void *)&str[i]);
+	}
+	return (NULL);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+int main()
+{
+    char str[] = "My name is Dilara-";
+    printf("My function: %s\n", ft_memchr(str, 'a',2));
+
+    char str2[] = "My name is Dilara-";
+    printf("Library function: %s", memchr(str2, 'a', 2));
+    return 0;
+}
+*/
