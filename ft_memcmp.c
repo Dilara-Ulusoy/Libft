45b13882c233883182ef5209d0ce3956f17c4491
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-24 10:26:05 +0300

    Add files via upload

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,21 +1,34 @@
-int ft_memcmp(const void *s1, const void *s2, size_t n)
-{
-  size_t diff;
-  size_t i;
-  unsigned char *str1;
-  unsigned char *str2;
-  str1 =(unsigned char *)s1;
-  str2 =(unsigned char *)s2;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dakcakoc <dakcakoce@student.hive.fi>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 10:23:57 by dakcakoc          #+#    #+#             */
+/*   Updated: 2024/04/24 10:25:32 by dakcakoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
 
-  i=0;
-  while (i < n)
-  {
-    diff = str1[i] - str2[i];
-    if (diff != 0 || str1[i] == '/0')
-      return(diff);
-    i++;
-  }
-  return (0);
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	diff;
+	size_t	i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		diff = str1[i] - str2[i];
+		if (diff != 0 || str1[i] == '/0')
+			return(diff);
+		i++;
+	}
+	return (0);
 }
 /*
 #include <stdio.h>
