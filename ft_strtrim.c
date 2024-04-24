3e7bb528aa882c42efcc4925c9489b298a0f3831
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-24 09:53:25 +0300

    Add files via upload

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,25 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dakcakoc <dakcakoce@student.hive.fi>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 09:50:10 by dakcakoc          #+#    #+#             */
+/*   Updated: 2024/04/24 09:53:05 by dakcakoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-   int end;
-   int start;
-   
-   start = 0;
-   if(!s1 && !set)
-       return (0);
-    if (*s1 == '\0' || *set == '\0')
-        return (ft_strdup(""));
-   while (s1[start] && ft_strchr(set, s1[start]))
-   {
-       start++;
-   }
-   end = ft_strlen(s1);
-   while (end > start && ft_strchr(set, s1[end - 1]))
-   {
-       end--;
-   }
-   return ft_substr(s1, start, end-start);
+	int	end;
+	int	start;
+
+	start = 0;
+	if (!s1 && !set)
+		return (0);
+	if (*s1 == '\0' || *set == '\0')
+		return (ft_strdup(""));
+	while (s1[start] && ft_strchr(set, s1[start]))
+	{
+		start++;
+	}
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+	{
+		end--;
+	}
+	return (ft_substr(s1, start, end - start));
 }
 /*
  #include <string.h>
