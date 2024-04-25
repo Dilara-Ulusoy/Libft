46e6187c042cd6c4bd35d3d1fef6cb0edc4d58ee
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-25 13:20:05 +0300

    Add files via upload

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -11,13 +11,13 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
 {
 	unsigned char		*d;
 	const unsigned char	*s;
 	size_t				i;
 
-	if (dest == NULL && src == NULL)
+	if (!dest && !src)
 		return (NULL);
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
