ed60c9b1ee5a770db224c9e1eb0ec34fa9ea206f
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-25 16:58:26 +0300

    Add files via upload

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,15 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dakcakoc <dakcakoce@student.hive.fi>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 16:56:54 by dakcakoc          #+#    #+#             */
+/*   Updated: 2024/04/25 16:57:49 by dakcakoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void	*ft_striteri (char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	char			*result;
 	unsigned int	i;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
-		result[i] = (*f)(i, s[i]);
+		(*f)(i, &s[i]);
 		i++;
 	}
-	result[i] = '\0';
 }
+/*
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+
+void func(unsigned int i, char *c)
+{
+    *c = *c - 32;
+}
+
+ int main()
+{
+    char str[10] = "dilara";
+    printf("Result before my function applied: %s\n", str);
+    ft_striteri(str, &func);
+    printf("Result after my function applied: %s\n", str);
+    return 0;
+}
+*/
