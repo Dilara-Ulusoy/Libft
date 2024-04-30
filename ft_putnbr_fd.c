1be280065a949e263827191c47e3999698c6bec7
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-04-30 16:11:24 +0300

    Add files via upload

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dakcakoc <dakcakoce@student.hive.fi>       +#+  +:+       +#+        */
+/*   By: dakcakoc <dakcakoc@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 09:45:29 by dakcakoc          #+#    #+#             */
-/*   Updated: 2024/04/19 12:41:16 by dakcakoc         ###   ########.fr       */
+/*   Updated: 2024/04/30 16:03:18 by dakcakoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_putnbr_fd(int n, int fd)
@@ -18,20 +19,22 @@ void	ft_putnbr_fd(int n, int fd)
 	if (n == -2147483648)
 	{
 		ft_putstr_fd("-2147483648", fd);
+		return ;
 	}
 	else if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
 		n = -n;
 	}
-	if (n >= 10 && n != -2147483648)
+	if (n >= 10)
 	{
 		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
-	}
-	else if (n != -2147483648)
-	{
 		digit = (n % 10) + '0';
+		ft_putchar_fd(digit, fd);
+	}
+	else
+	{
+		digit = n + '0';
 		ft_putchar_fd(digit, fd);
 	}
 }
