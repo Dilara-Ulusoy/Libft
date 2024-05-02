f231d9e6a6678426d39c6491ffb9cfa1fe1c408a
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-05-02 13:52:12 +0300

    Add files via upload

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dakcakoc <dakcakoce@student.hive.fi>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 14:01:47 by dakcakoc          #+#    #+#             */
-/*   Updated: 2024/04/29 14:04:54 by dakcakoc         ###   ########.fr       */
+/*   Updated: 2024/05/02 13:51:34 by dakcakoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,23 +15,26 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
 	t_list	*new_node;
+	void	*content;
 
-	if (!lst || !f || !del)
-		return (NULL);
 	new_lst = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
-		if (!new_node)
+		content = f(lst->content);
+		if (!content)
 		{
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
-		else
+		new_node = ft_lstnew(content);
+		if (!new_node)
 		{
-			ft_lstadd_back(&new_lst, new_node);
-			lst = lst->next;
+			del(content);
+			ft_lstclear(&new_lst, del);
+			return (NULL);
 		}
+		ft_lstadd_back(&new_lst, new_node);
+		lst = lst->next;
 	}
 	return (new_lst);
 }
