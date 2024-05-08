114b4cc5124c31d131d804a325545d813cb11b0c
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-05-08 11:54:24 +0300

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -30,6 +30,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 /*
+This function copies n bytes from memory area src 
+to memory area dest.
+*/
+/*
 #include <stdio.h>
 #include <string.h>
 int main()
