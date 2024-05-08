6e2b1c85c0875f273d0551c73945627ace44282b
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-05-08 11:44:22 +0300

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -43,3 +43,17 @@ int main()
     printf("Library function: %s", memchr(str2, 'a', 5));
     return 0;
 }*/
+/*
+This function locates the first occurrence of c 
+in the first n bytes of the string pointed to by s.
+If c is found, the function returns memory address of index i where c is found.
+Unsigned char represents a byte of memory. 
+By casting to unsigned char, we can access the memory block byte by byte,
+which is necessary for searching for character (c) within the memory block.
+By casting int to unsigned char, we ensure that 
+we compare the data in the memory block properly 
+because the range of unsigned char is 0 to 255, 
+and all values are positive. 
+This allows us to accurately compare each byte of the memory block 
+with the character c
+*/
